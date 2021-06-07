ebdf0125481278af0af47e1a03872411b1f3d70e
Manal Ateyyat <manalateyyat4@gmail.com>
2021-06-07 13:32:13 +0300

    Class11 updated

--- App2.js
+++ App2.js
@@ -6,17 +6,17 @@ let firstImg=document.getElementById('first');
 let secondImg=document.getElementById('second'); 
 let thirdImg=document.getElementById('third');
 
-let maxtrials=4;
+let maxtrials=25;
 let userNumTrial=0; 
 let firstImageIndex;
 let secondImageIndex;
 let thirdImageIndex;
 
 function Product(name,source) {  
-    this.name=name;
-    this.source=source;
-    this.showntimes=0;
-    this.votes=0;
+this.name=name;
+this.source=source;
+this.showntimes=0;
+this.votes=0;
     Product.allproducts.push(this);
     }
     Product.allproducts=[];
@@ -25,22 +25,22 @@ new Product('bag','img/assets/bag.jpg');
 new Product('banana','img/assets/banana.jpg');
 new Product('bathroom','img/assets/bathroom.jpg');
 new Product('boots','img/assets/boots.jpg');
-// new Product('breakfast','img/assets/breakfast.jpg');
-// new Product('bubblegum','img/assets/bubblegum.jpg');
-// new Product('chair','img/assets/chair.jpg');
-// new Product('cthulhu','img/assets/cthulhu.jpg');
-// new Product('dog-duck','img/assets/dog-duck.jpg');
-// new Product('dragon','img/assets/dragon.jpg');
-// new Product('pen','img/assets/pen.jpg');
-// new Product('pet-sweep','img/assets/pet-sweep.jpg');
-// new Product('scissors','img/assets/scissors.jpg');
-// new Product('shark','img/assets/shark.jpg');
-// new Product('sweep','img/assets/sweep.png');
-// new Product('tauntaun','img/assets/tauntaun.jpg');
-// new Product('unicorn','img/assets/unicorn.jpg');
-// new Product('usb','img/assets/usb.gif');
-// new Product('water-can','img/assets/water-can.jpg');
-// new Product('wine-glass','img/assets/wine-glass.jpg');
+new Product('breakfast','img/assets/breakfast.jpg');
+new Product('bubblegum','img/assets/bubblegum.jpg');
+new Product('chair','img/assets/chair.jpg');
+new Product('cthulhu','img/assets/cthulhu.jpg');
+new Product('dog-duck','img/assets/dog-duck.jpg');
+new Product('dragon','img/assets/dragon.jpg');
+new Product('pen','img/assets/pen.jpg');
+new Product('pet-sweep','img/assets/pet-sweep.jpg');
+new Product('scissors','img/assets/scissors.jpg');
+new Product('shark','img/assets/shark.jpg');
+new Product('sweep','img/assets/sweep.png');
+new Product('tauntaun','img/assets/tauntaun.jpg');
+new Product('unicorn','img/assets/unicorn.jpg');
+new Product('usb','img/assets/usb.gif');
+new Product('water-can','img/assets/water-can.jpg');
+new Product('wine-glass','img/assets/wine-glass.jpg');
 
 
 function generateRandomIndex()
